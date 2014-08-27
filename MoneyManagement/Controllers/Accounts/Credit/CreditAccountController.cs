5086574813b44f899bc52926c2a5ff495510aa22
Andres Borquez <andres.bp@gmail.com>
2014-08-27 07:26:12 -0700

    Adecuaciones en el modelo de datos y creacion de Create y Get dummies para CreditAccount.

---
+++ MoneyManagement/Controllers/Accounts/Credit/CreditAccountController.cs
@@ -0,0 +1,45 @@
+﻿using MoneyManagement.BusinessLayer.Manager;
+using MoneyManagement.Contracts.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MoneyManagement.Controllers.Accounts.Credit
+{
+    public class CreditAccountController : Controller
+    {
+        // GET: CreditAccount
+        public ActionResult Index()
+        {
+            var creditAccountManager = new CreditAccountManager();
+
+            var creditAccounts = creditAccountManager.Get();
+
+            return View(creditAccounts);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(CreditAccount creditAccount)
+        {
+            var creditAccountManager = new CreditAccountManager();
+
+            if (ModelState.IsValid)
+            {
+                creditAccountManager.Save(creditAccount);
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(creditAccount);
+            }
+        }
+    }
+}
